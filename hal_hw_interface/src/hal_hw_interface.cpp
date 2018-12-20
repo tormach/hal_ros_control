75e4f7b9356d93137764988c0370deeb4b7bb14c
Alexander Rössler <mail@roessler.systems>
2018-12-20 20:02:11 +0000

    QA: apply ROS clang-format

--- hal_hw_interface/src/hal_hw_interface.cpp
+++ hal_hw_interface/src/hal_hw_interface.cpp
@@ -1,13 +1,10 @@
 #include <hal_hw_interface/hal_hw_interface.h>
 #include <hal_hw_interface/hal_ros_logging.h>
 
-
 namespace hal_hw_interface
 {
-
-HalHWInterface::HalHWInterface(
-  ros::NodeHandle &nh, urdf::Model *urdf_model)
-: ros_control_boilerplate::GenericHWInterface(nh, urdf_model)
+HalHWInterface::HalHWInterface(ros::NodeHandle& nh, urdf::Model* urdf_model)
+  : ros_control_boilerplate::GenericHWInterface(nh, urdf_model)
 {
 }
 
@@ -25,7 +22,7 @@ void HalHWInterface::init(void (*funct)(void*, long))
   if (comp_id_ < 0)
   {
     HAL_ROS_LOG_ERR(CNAME, "%s:  ERROR: Component creation ABORTED", CNAME);
-    //return false; // FIXME
+    // return false; // FIXME
     return;
   }
 
@@ -35,8 +32,7 @@ void HalHWInterface::init(void (*funct)(void*, long))
   for (std::size_t ix = 0; ix < num_joints_; ix++)
   {
     // init_joint(ix);
-    HAL_ROS_LOG_INFO(
-      CNAME, "%s: Init joint #%zu %s", CNAME, ix, joint_names_[ix].c_str());
+    HAL_ROS_LOG_INFO(CNAME, "%s: Init joint #%zu %s", CNAME, ix, joint_names_[ix].c_str());
 
     if (!create_float_pin(ix, &joint_pos_cmd_ptrs_, HAL_OUT, "pos-cmd") ||
         !create_float_pin(ix, &joint_vel_cmd_ptrs_, HAL_OUT, "vel-cmd") ||
@@ -45,10 +41,8 @@ void HalHWInterface::init(void (*funct)(void*, long))
         !create_float_pin(ix, &joint_vel_fb_ptrs_, HAL_IN, "vel-fb") ||
         !create_float_pin(ix, &joint_eff_fb_ptrs_, HAL_IN, "eff-fb"))
     {
-      HAL_ROS_LOG_ERR(
-        CNAME, "%s: Failed to initialize joint %zu %s.%s",
-        CNAME, ix, CNAME, joint_names_[ix].c_str());
-      //return false; // FIXME
+      HAL_ROS_LOG_ERR(CNAME, "%s: Failed to initialize joint %zu %s.%s", CNAME, ix, CNAME, joint_names_[ix].c_str());
+      // return false; // FIXME
       return;
     }
   }
@@ -59,7 +53,7 @@ void HalHWInterface::init(void (*funct)(void*, long))
   {
     HAL_ROS_LOG_INFO(CNAME, "%s: ERROR: hal_export_functf failed", CNAME);
     hal_exit(comp_id_);
-    //return false; // FIXME
+    // return false; // FIXME
     return;
   }
   HAL_ROS_LOG_INFO(CNAME, "%s:  Exported HAL function", CNAME);
@@ -69,54 +63,48 @@ void HalHWInterface::init(void (*funct)(void*, long))
 
   HAL_ROS_LOG_INFO(CNAME, "%s:  HAL component ready!", CNAME);
 
-  //return true; // FIXME
-} // init()
+  // return true; // FIXME
+}  // init()
 
-bool HalHWInterface::create_float_pin(
-  const std::size_t ix, std::vector<double**> *ptrs,
-  hal_pin_dir_t dir, const char* name)
+bool HalHWInterface::create_float_pin(const std::size_t ix, std::vector<double**>* ptrs, hal_pin_dir_t dir,
+                                      const char* name)
 {
   // Sanity check vector length
   if (ptrs->size() != ix)
   {
-    HAL_ROS_LOG_ERR(
-      CNAME, "%s: Size of pin storage not consistent with ID", CNAME);
+    HAL_ROS_LOG_ERR(CNAME, "%s: Size of pin storage not consistent with ID", CNAME);
     return false;
   }
   // Allocate space
-  ptrs->push_back((hal_float_t**) hal_malloc(sizeof(hal_float_t*)));
-  if (ptrs->at(ix) == NULL) {
+  ptrs->push_back((hal_float_t**)hal_malloc(sizeof(hal_float_t*)));
+  if (ptrs->at(ix) == NULL)
+  {
     HAL_ROS_LOG_ERR(CNAME, "%s: Allocate HAL pin failed", CNAME);
     return false;
   }
-  if (hal_pin_float_newf(dir, ptrs->at(ix), comp_id_,
-                         "%s.%s.%s", CNAME, joint_names_[ix].c_str(), name))
+  if (hal_pin_float_newf(dir, ptrs->at(ix), comp_id_, "%s.%s.%s", CNAME, joint_names_[ix].c_str(), name))
   {
-    HAL_ROS_LOG_INFO(
-      CNAME, "%s: New HAL pin %s.%s.%s failed",
-      CNAME, CNAME, joint_names_[ix].c_str(), name);
+    HAL_ROS_LOG_INFO(CNAME, "%s: New HAL pin %s.%s.%s failed", CNAME, CNAME, joint_names_[ix].c_str(), name);
     return false;
   }
 
-  HAL_ROS_LOG_INFO(
-    CNAME, "%s: New HAL pin %s.%s.%s succeeded; addr %p",
-    CNAME, CNAME, joint_names_[ix].c_str(), name, ptrs->at(ix));
+  HAL_ROS_LOG_INFO(CNAME, "%s: New HAL pin %s.%s.%s succeeded; addr %p", CNAME, CNAME, joint_names_[ix].c_str(), name,
+                   ptrs->at(ix));
   return true;
 }
 
-
-void HalHWInterface::read(ros::Duration &elapsed_time)
+void HalHWInterface::read(ros::Duration& elapsed_time)
 {
   // Copy HAL joint feedback pin values to controller joint states
   for (std::size_t joint_id = 0; joint_id < num_joints_; ++joint_id)
   {
     joint_position_[joint_id] = **joint_pos_fb_ptrs_[joint_id];
     joint_velocity_[joint_id] = **joint_vel_fb_ptrs_[joint_id];
-    joint_effort_[joint_id]   = **joint_eff_fb_ptrs_[joint_id];
+    joint_effort_[joint_id] = **joint_eff_fb_ptrs_[joint_id];
   }
 }
 
-void HalHWInterface::write(ros::Duration &elapsed_time)
+void HalHWInterface::write(ros::Duration& elapsed_time)
 {
   // Enforce joint limits
   enforceLimits(elapsed_time);
@@ -129,24 +117,25 @@ void HalHWInterface::write(ros::Duration &elapsed_time)
   }
 }
 
-
-void HalHWInterface::enforceLimits(ros::Duration &period)
+void HalHWInterface::enforceLimits(ros::Duration& period)
 {
   // FIXME how does this fit in?  Should it just be done in HAL?
   // from sim_hw_interface.cpp:
-  //pos_jnt_sat_interface_.enforceLimits(period);
+  // pos_jnt_sat_interface_.enforceLimits(period);
 }
 
 void HalHWInterface::shutdown()
 {
-  if (! comp_id_)
+  if (!comp_id_)
   {
     HAL_ROS_LOG_ERR(CNAME, "%s: HAL already shut down", CNAME);
-  } else {
+  }
+  else
+  {
     HAL_ROS_LOG_INFO(CNAME, "%s: HAL shutting down", CNAME);
     hal_exit(comp_id_);
     comp_id_ = 0;
   }
 }
 
-} // namespace
+}  // namespace
