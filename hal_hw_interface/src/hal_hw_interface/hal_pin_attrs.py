672dd50d2308071d799750025b1f73e9afdcd3d8
John Morris <john@zultron.com>
2019-02-06 13:37:05 +0800

    hal_hw_interface:  New hal_hw_interface.hal_pin_attrs module
    These objects represent human-friendly and config-file-friendly pin
    direction `HAL_IN`, `HAL_OUT` and pin type `HAL_BIT`, `HAL_FLOAT`,
    etc. constants.  They subclass `int`, and the constructor can
    initialize with strings, both short and long e.g. `IN` and
    `HAL_FLOAT`, and their `str()` and `repr()` methods are similarly
    readable.

---
+++ hal_hw_interface/src/hal_hw_interface/hal_pin_attrs.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import hal
+
+
+class HalPinAttrBase(int):
+    '''Subclass int to make a simple interface for accessing
+    hal.HAL_<suffix> enums by integer or string
+    '''
+
+    _suffixes = []  # hal.HAL_%s; define in subclasses
+    _updated = False
+
+    # Maps for translating constructor input and human-readable output
+    _fwd_map = dict()  # 16 -> 'IO'
+    _bwd_map = dict()  # 'HAL_IN' -> 16; 'IN' -> 16
+    for attr in dir(hal):
+        if not attr.startswith('HAL_'):
+            continue
+        value = getattr(hal, attr)
+        attr_short = attr[4:]
+        _fwd_map[value] = attr_short
+        _bwd_map[attr_short] = value
+
+    def __new__(cls, value):
+        '''Create new object, translating strings to ints and validating value
+        '''
+        if isinstance(value, int):
+            if cls._fwd_map.get(value, None) not in cls._suffixes:
+                raise ValueError("Illegal value '{}'".format(value))
+            return int.__new__(cls, value)
+        elif isinstance(value, str):
+            if value.startswith('HAL_'):
+                value = value[4:]
+            if value not in cls._suffixes:
+                raise ValueError("Illegal value '{}'".format(value))
+            return int.__new__(cls, cls._bwd_map[value])
+        else:
+            raise ValueError("Illegal value '{}'".format(value))
+
+    def __repr__(self):
+        return 'HAL_' + self._fwd_map[self]
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class HalPinDir(HalPinAttrBase):
+    """A HAL pin direction
+
+    This :py:class:`int` type comes from the :py:mod:`HAL` module
+    :code:`HAL_IN`, :code:`HAL_OUT`, :code:`HAL_IO` attributes.  These
+    may be specified in the constructor as the short or long string,
+    e.g. :code:`'OUT'` or :code:`'HAL_IO'`.
+    """
+
+    _suffixes = set(['IN', 'OUT', 'IO'])
+
+
+class HalPinType(HalPinAttrBase):
+    """A HAL pin type
+
+    This :py:class:`int` type comes from the :py:mod:`HAL` module
+    :code:`HAL_BIT`, :code:`HAL_U32`, :code:`HAL_S32`,
+    :code:`HAL_FLOAT` attributes.  These may be specified in the
+    constructor as the short or long string, e.g. :code:`'BIT'` or
+    :code:`'HAL_FLOAT'`.
+    """
+
+    _suffixes = set(['BIT', 'U32', 'S32', 'FLOAT'])
---
+++ hal_hw_interface/src/hal_hw_interface/tests/test_hal_pin_attrs.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import pytest
+import hal
+
+from hal_hw_interface.hal_pin_attrs import HalPinDir, HalPinType
+
+valid_cases = [
+    # Pin direction
+    (HalPinDir, 'IN', hal.HAL_IN),
+    (HalPinDir, 'OUT', hal.HAL_OUT),
+    (HalPinDir, 'IO', hal.HAL_IO),
+    # Pin type
+    (HalPinType, 'BIT', hal.HAL_BIT),
+    (HalPinType, 'U32', hal.HAL_U32),
+    (HalPinType, 'S32', hal.HAL_S32),
+    (HalPinType, 'FLOAT', hal.HAL_FLOAT),
+]
+
+
+@pytest.fixture(params=valid_cases)
+def valid_case(request):
+    return request.param
+
+
+def test_hal_pin_attrs_valid_case_fixture(valid_case):
+    print(valid_case)
+    assert len(valid_case) == 3
+    obj_type, short_name, int_val = valid_case
+    assert obj_type in (HalPinDir, HalPinType)
+    assert short_name in ('IN', 'OUT', 'IO', 'BIT', 'U32', 'S32', 'FLOAT')
+
+
+def test_hal_pin_attr_new_from_short_name(valid_case):
+    # Test init from short name, e.g. 'IN'
+    obj_type, short_name, int_val = valid_case
+    test_val = obj_type(short_name)
+    assert test_val == int_val
+
+
+def test_hal_pin_attr_new_from_long(valid_case):
+    # Test init from long name, e.g. 'HAL_IN'
+    obj_type, short_name, int_val = valid_case
+    test_val = obj_type('HAL_' + short_name)
+    assert test_val == int_val
+
+
+def test_hal_pin_attr_new_from_hal_enum(valid_case):
+    # Test init from integer, e.g. (hal.HAL_IN)
+    obj_type, short_name, int_val = valid_case
+    test_val = obj_type(int_val)
+    assert test_val == int_val
+
+
+def test_hal_pin_attr_repr(valid_case):
+    # Test __repr__ returns e.g. 'HAL_IN'
+    obj_type, short_name, int_val = valid_case
+    test_val = obj_type(int_val)
+    assert repr(test_val) == 'HAL_' + short_name
+
+
+def test_hal_pin_attr_str(valid_case):
+    # Test __str__ returns e.g. 'HAL_IN'
+    obj_type, short_name, int_val = valid_case
+    test_val = obj_type(int_val)
+    assert str(test_val) == 'HAL_' + short_name
+
+
+invalid_cases = [
+    # Pin direction
+    (HalPinDir, 'INY'),
+    (HalPinDir, 'in'),
+    (HalPinDir, hal.HAL_BIT),
+    (HalPinDir, hal.HAL_U32),
+    (HalPinDir, hal.HAL_S32),
+    (HalPinDir, hal.HAL_FLOAT),
+    (HalPinDir, -1),
+    (HalPinDir, None),
+    (HalPinDir, float(hal.HAL_IN)),
+    # Pin type
+    (HalPinType, 'BITY'),
+    (HalPinType, 'bit'),
+    (HalPinType, hal.HAL_IN),
+    (HalPinType, hal.HAL_OUT),
+    (HalPinType, hal.HAL_IO),
+    (HalPinType, -1),
+    (HalPinType, None),
+    (HalPinType, float(hal.HAL_BIT)),
+]
+
+
+@pytest.fixture(params=invalid_cases)
+def invalid_case(request):
+    return request.param
+
+
+def test_hal_pin_attrs_invalid_case_fixture(invalid_case):
+    print(invalid_case)
+    assert len(invalid_case) == 2
+    obj_type, bogus_value = invalid_case
+    assert obj_type in (HalPinDir, HalPinType)
+
+
+def test_hal_attr_type_new_invalid(invalid_case):
+    # Test init from invalid values raises ValueError
+    obj_type, bogus_value = invalid_case
+    with pytest.raises(ValueError):
+        obj_type(bogus_value)
