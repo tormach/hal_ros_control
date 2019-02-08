5e8a110461908632a5290aed1f48b0be9546ba40
John Morris <john@zultron.com>
2019-02-08 15:29:40 +0800

    hal_hw_interface:  Add hal_hw_interface.redis_store_hal_pin module
    Add a new ROS HAL pin type that persists its value in a redis_store
    key.  The component may call pin methods to set the pin value from
    redis and vice-versa.

---
+++ hal_hw_interface/src/hal_hw_interface/redis_store_hal_pin.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+   :synopsis: HAL pin object connected to `redis_store` parameter
+     server for use in :py:mod:`hal_hw_interface.ros_hal_component`
+
+   .. moduleauthor:: John Morris <john@dovetail-automata.com>
+
+"""
+
+import rospy
+
+# Mock patching breaks with `from redis_store import ConfigClient`; why?
+import redis_store.config as redis_config
+
+from hal_hw_interface.ros_hal_pin import RosHalPin
+
+
+class RedisStoreHalPin(RosHalPin):
+    '''HAL pin attached to :code:`redis_store` ROS package's parameter
+    server
+
+    This HAL pin's value may be read from and written to a
+    :code:`redis_store` parameter server.  The default key name is
+    :code:`<compname>/<name>`.
+
+    :param name: The HAL pin name
+    :type name: str
+    :param hal_type: HAL pin data type, one of :code:`['BIT', 'U32',
+      'S32', 'FLOAT']`
+    :type hal_type:  :py:class:`hal_hw_interface.hal_pin_attrs.HalPinType`
+    :param hal_dir: HAL pin direction, one of :code:`['IN', 'OUT', 'IO']`
+    :type hal_dir:  :py:class:`hal_hw_interface.hal_pin_attrs.HalPinDir`
+    '''
+
+    # Usually (?) read output pin default value from redis
+    _default_hal_dir = 'OUT'
+
+    @property
+    def _redis_config(self):
+        if 'redis_config_client' not in self._cached_objs:
+            # Autovivify
+            c = redis_config.ConfigClient()
+            self._cached_objs['redis_config_client'] = c
+        return self._cached_objs['redis_config_client']
+
+    @property
+    def _redis_param_key(self):
+        return "{}/{}".format(self.compname, self.pin_name)
+
+    def set_pin_from_redis(self, default=None):
+        '''Read and return value from redis_store
+        '''
+        redis_value = self._redis_config.get_param(self._redis_param_key)
+        if redis_value is None and default is None:
+            rospy.logwarn(
+                "%s:  Unable to initialize offset:  "
+                "no saved value and no default given" % self.compname
+            )
+            return
+        new_value = default if redis_value is None else redis_value
+        self.set_pin(new_value)
+        return new_value
+
+    def set_redis_from_pin(self):
+        '''Read HAL pin value and write to redis_store
+        '''
+        old_val = self._redis_config.get_param(self._redis_param_key)
+        new_val = self.get_pin()
+        self._redis_config.set_param(self._redis_param_key, new_val)
+        if old_val is not None and not self._isclose(
+            new_val, old_val, 1e-5, 1e-5
+        ):
+            rospy.loginfo(
+                "%s:  Updated %s from %.4f to %.4f",
+                self.compname,
+                self._redis_param_key,
+                old_val,
+                self._redis_config.get_param(self._redis_param_key),
+            )
+        return new_val
+
+    @classmethod
+    def shutdown(cls):
+        config = cls._cached_objs.get('redis_config_client', None)
+        if config is not None:
+            config.stop()
---
+++ hal_hw_interface/src/hal_hw_interface/tests/test_redis_store_hal_pin.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+from hal_hw_interface.redis_store_hal_pin import RedisStoreHalPin
+
+# Borrow tests from ros_hal_pin
+from test_ros_hal_pin import TestRosHalPin, HalPinDir
+
+
+class TestRedisStoreHalPin(TestRosHalPin):
+    default_hal_dir = HalPinDir('OUT')
+    test_class = RedisStoreHalPin
+
+    def redis_param_key(self, obj):
+        return "{}/{}".format(obj.compname, obj.pin_name)
+
+    def set_mock_redis_param(self, obj, param):
+        if isinstance(param, dict):
+            value = self.other_value(obj, param)
+        else:
+            value = param
+        obj._redis_config.set_key(self.redis_param_key(obj), value)
+        return value
+
+    def test_redis_store_pin_param_key(self, obj):
+        assert obj._redis_param_key == self.redis_param_key(obj)
+
+    def test_redis_store_pin_client(self, mock_redis_client_obj, mock_comp_obj):
+        # Check that redis client is created and cached
+        self.setup_hal_obj_base(mock_comp_obj)
+        c1 = self.test_class('test_redis_pin', 'FLOAT')._redis_config
+        assert c1 is mock_redis_client_obj
+        c2 = self.test_class('test_redis_pin2', 'FLOAT')._redis_config
+        assert c1 is c2
+
+    def test_redis_store_pin_set_pin_from_redis(
+        self, obj, data, mock_comp_obj, mock_redis_client_obj
+    ):
+        # Fake a redis param value and set the pin from redis
+        test_val = self.set_mock_redis_param(obj, data)
+        obj_val = obj.set_pin_from_redis()
+        # Check that redis was read
+        mock_redis_client_obj.get_param.assert_called_once_with(
+            '{}/{}'.format(self.compname, self.obj_test_name(obj))
+        )
+        # Check the returned value
+        assert obj_val == test_val
+        # Check the HAL pin value
+        mock_comp_obj.__setitem__.assert_called_with(
+            self.obj_test_name(obj), test_val
+        )
+
+    def test_redis_store_pin_set_pin_no_defaults(self, all_patches):
+        mock_comp_obj, mock_rospy, mock_objs, mock_redis_client_obj = (
+            all_patches
+        )
+        # Fake a redis param value and set the pin from redis
+        obj = self.test_class('unconfigured_redis_pin', 'FLOAT')
+        self.setup_hal_obj_base(mock_comp_obj)
+        self.set_mock_redis_param(obj, None)
+        mock_redis_client_obj.reset_mock()
+        # Call set_pin_from_redis() and check that the pin was NOT set
+        print("--------- Calling method")
+        obj.set_pin_from_redis()
+        print(mock_comp_obj.__setitem__.mock_calls)
+        mock_comp_obj.__setitem__.assert_not_called()
+
+    def test_redis_store_pin_set_redis_from_pin(
+        self, obj, data, mock_redis_client_obj
+    ):
+        # Fake a HAL pin value, set redis key, clean up
+        test_val = self.set_pin(obj, data)
+        self.set_mock_redis_param(obj, data)
+        mock_redis_client_obj.reset_mock()
+        # Call the method
+        print("--------- Calling method")
+        obj_val = obj.set_redis_from_pin()
+        # Check the returned value
+        assert obj_val == test_val
+        # Check redis was written to
+        mock_redis_client_obj.set_param.assert_called_once_with(
+            '{}/{}'.format(self.compname, self.obj_test_name(obj)), test_val
+        )
+        # Check if log message was printed
+        print(mock_redis_client_obj.get_param.mock_calls)
+        if data['changed']:
+            # Call get_param to check value and again to print log
+            assert mock_redis_client_obj.get_param.call_count == 2
+        else:
+            # Only call get_param once to check value, not again to print log
+            assert mock_redis_client_obj.get_param.call_count == 1
+
+    def test_redis_store_pin_set_redis_from_pin_first_time(
+        self, obj, data, mock_redis_client_obj
+    ):
+        # Fake a HAL pin value, set redis key to None, clean up
+        test_val = self.set_pin(obj, data)
+        self.set_mock_redis_param(obj, None)
+        mock_redis_client_obj.reset_mock()
+        # Call the method
+        obj_val = obj.set_redis_from_pin()
+        # Check the returned value
+        assert obj_val == test_val
+        # Check redis was written to
+        mock_redis_client_obj.set_param.assert_called_once_with(
+            '{}/{}'.format(self.compname, self.obj_test_name(obj)), test_val
+        )
+        # Check the log message was not printed
+        print(mock_redis_client_obj.get_param.mock_calls)
+        assert mock_redis_client_obj.get_param.call_count == 1
