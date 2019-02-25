04ca50c571581fa5ce3fbf11c32cd1aa8141e784
John Morris <john@zultron.com>
2019-02-26 05:58:24 +0800

    Document hal_hw_interface.redis_store_hal_pin.shutdown()

--- hal_hw_interface/src/hal_hw_interface/redis_store_hal_pin.py
+++ hal_hw_interface/src/hal_hw_interface/redis_store_hal_pin.py
@@ -85,6 +85,10 @@ class RedisStoreHalPin(RosHalPin):
 
     @classmethod
     def shutdown(cls):
+        '''Closes the shared redis client connection.  Call this from
+        :py:func:`hal_hw_interface.ros_hal_component.RosHalComponent.shutdown_component`
+        to close the connection at the HAL component's exit.
+        '''
         config = cls._cached_objs.get('redis_config_client', None)
         if config is not None:
             config.stop()
