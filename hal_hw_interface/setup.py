39735f5ffc22f578bdba85f7cd9a57a95525d85d
John Morris <john@zultron.com>
2021-12-13 21:49:13 -0600

    foxy WIP:  black formatter

--- hal_hw_interface/setup.py
+++ hal_hw_interface/setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup
 
-package_name = 'hal_hw_interface'
+package_name = "hal_hw_interface"
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version="0.1.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            "share/ament_index/resource_index/packages",
+            ["resource/" + package_name],
+        ),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='John Morris',
-    maintainer_email='john@zultron.com',
-    description='Machinekit HAL interface to robot hardware and I/O',
-    license='BSD',
-    tests_require=['pytest'],
+    maintainer="John Morris",
+    maintainer_email="john@zultron.com",
+    description="Machinekit HAL interface to robot hardware and I/O",
+    license="BSD",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "scripts/hal_io",
             "scripts/hal_mgr",
         ],
