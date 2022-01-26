21550a9340a30e3d4b49edae7124aa88d9b136f5
John Morris <john@zultron.com>
2022-01-26 16:29:09 -0600

    hal_hw_interface:  Update hal_mgr to work with new launch system

--- hal_hw_interface/hal_hw_interface/hal_mgr.py
+++ hal_hw_interface/hal_hw_interface/hal_mgr.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from machinekit import config, rtapi
+from machinekit import config
 from .ros_hal_component import RosHalComponent
 
 # ROS
@@ -20,7 +20,6 @@ class HalMgr(RosHalComponent):
         # super().setup_component()
         self.init_ready_topic_publisher()
         self.start_realtime()
-        self.load_hal_config()
 
     def init_ready_topic_publisher(self):
         """Set up "ready" status publisher."""
@@ -35,12 +34,21 @@ class HalMgr(RosHalComponent):
         Start Machinekit `realtime` environment.  Register the
         :py:func:`stop_realtime` shutdown callback.
         """
-        os.environ.setdefault("DEBUG", "0")
-        if "MACHINEKIT_INI" not in os.environ:  # export for package installs
-            mkconfig = config.Config()
-            os.environ["MACHINEKIT_INI"] = mkconfig.MACHINEKIT_INI
-        subprocess.check_call("realtime start", shell=True)
+        # Set up console debug output and config
+        d_out = "1" if self.get_ros_param("hal_debug_output", True) else "0"
+        d_lev = str(self.get_ros_param("hal_debug_level", 1))
+        self.logger.info(f"Starting hal_mgr; debug={d_out}/level={d_lev}")
+        env = dict(
+            DEBUG=d_lev,
+            SYSLOG_TO_STDERR=d_out,
+            MACHINEKIT_INI=config.Config().MACHINEKIT_INI,
+            **os.environ,
+        )
+        subprocess.check_call(["realtime", "start"], env=env)
         self.add_shutdown_callback(self.stop_realtime, 999)  # run last
+
+        # Signal readiness
+        self.publish_ready_topic(value=True)
         self.logger.info("hal_mgr:  Started realtime")
 
     def stop_realtime(self):
@@ -48,60 +56,21 @@ class HalMgr(RosHalComponent):
         subprocess.check_call("realtime stop", shell=True)
         self.logger.info("Realtime stopped")
 
-    def load_hal_config(self):
-        # Be sure rtapi is initialized (used in python HAL configs)
-        if not getattr(rtapi, "__rtapicmd") or not rtapi.__rtapicmd:
-            rtapi.init_RTAPI()
-
-        # Load the hal_hw_interface
-        hal_comp_dir = self.get_ros_param("hal_comp_dir")
-        assert hal_comp_dir
-        hal_hw_interface_path = os.path.join(hal_comp_dir, "hal_control_node")
-        self.logger.info(f"hal_hw_interface_path:  {hal_hw_interface_path}")
-        rtapi.loadrt(hal_hw_interface_path, ARGV=",".join(self.argv))
-        self.logger.info("rosctl_intf comp loaded successfully")
-
-        # Get HAL config directory and filename list from ROS parameters
-        hal_file_dir = self.get_ros_param("hal_file_dir")
-        assert hal_file_dir is not None
-        hal_files = self.get_ros_param("hal_files")
-        assert isinstance(hal_files, list)
-
-        # Munge directory and filenames into paths
-        hal_file_paths = list()
-        for hal_file in hal_files:
-            hal_file_path = os.path.join(hal_file_dir, hal_file)
-            assert os.path.exists(hal_file_path)
-            hal_file_paths.append(hal_file_path)
-
-        # Load HAL configuration from HAL files
-        for fpath in hal_file_paths:
-            self.logger.info(f"Loading hal file '{fpath}'")
-            if fpath.endswith(".py"):
-                with open(fpath, "r") as f:
-                    data = compile(f.read(), fpath, "exec")
-                globals_ = {}
-                exec(data, globals_)
-            else:
-                subprocess.check_call(["halcmd", "-f", fpath])
-
-        # Signal readiness
-        self.publish_ready_topic(value=True)
-        self.logger.info("HAL configuration loaded")
-
     def update(self):
+        # FIXME Watch realtime; shut down if it shuts down
         self.publish_ready_topic()
 
     def publish_ready_topic(self, value=None):
         """
         Publish the HAL config "ready" status.
 
-        This topic coming `True` indicates the HAL configuration is
-        complete.
+        This topic coming `True` indicates RTAPI/HAL is running.
 
         Change published value by setting the `value` param to `True`
         or `False.
         """
         if value is not None:
+            if self.ready_msg.data != value:
+                self.logger.info(f"hal_mgr/ready topic new value:  {value}")
             self.ready_msg.data = value
         self.ready_pub.publish(self.ready_msg)
