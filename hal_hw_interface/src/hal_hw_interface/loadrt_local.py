c38ac87906a32a3c502fc1c96549c45e7ca03a9c
John Morris <john@zultron.com>
2022-08-29 14:19:03 -0500

    hal_hw_interface:  Pass args through `loadrt_local()`
    This commit passes `loadrt` args to support V1 comps.

--- hal_hw_interface/src/hal_hw_interface/loadrt_local.py
+++ hal_hw_interface/src/hal_hw_interface/loadrt_local.py
@@ -3,17 +3,20 @@ import os
 from machinekit import rtapi, hal
 
 
-def loadrt_local(modname):
-    """Load a locally-built HAL component not installed in the standard
-    module directory
+def loadrt_local(modname, *args, **kwargs):
+    """
+    Load a RT HAL component.
+
+    Load a locally-built HAL component installed outside standard module
+    directories.  Uses the `LD_LIBRARY_PATH` environment variable as a
+    search path.
     """
     if modname in hal.components:
         return
-    for path in os.environ.get('LD_LIBRARY_PATH', '').split(':'):
-        rospy.logdebug(f"Checking for {modname}.so in {path}")
-        modpath = os.path.join(path, f'{modname}')
-        if os.path.exists(f'{modpath}.so'):
+    for path in os.environ.get("LD_LIBRARY_PATH", "").split(":"):
+        modpath = os.path.join(path, f"{modname}")
+        if os.path.exists(f"{modpath}.so"):
             break
     else:
-        raise RuntimeError(f'Unable to locate {modname} module')
-    rtapi.loadrt(modpath)
+        raise RuntimeError(f"Unable to locate {modname} module")
+    rtapi.loadrt(modpath, *args, **kwargs)
